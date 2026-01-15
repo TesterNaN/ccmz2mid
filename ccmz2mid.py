ee92c1b841f2c2aa620e59ce7ad5b923773107c8
TesterNaN <88355571+TesterNaN@users.noreply.github.com>
2026-01-16 05:00:26 +0800

    Update ccmz2mid.py

--- ccmz2mid.py
+++ ccmz2mid.py
@@ -3,7 +3,7 @@
 
 """
 ccmz2mid - 虫虫钢琴ccmz文件转midi工具
-Copyright (C) 2025 TesterNaN
+Copyright (C) 2026 TesterNaN
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
